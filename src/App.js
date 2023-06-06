90a090da6ff67b0fa01e77a2f695b3cfafcc04a4
Amal E R <amaler@Amals-MacBook-Air.local>
2023-06-06 11:21:01 +0530

    furnitures project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import components
+import Hero from './components/Hero'
+import  Features from './components/Features'
+import  NewItems from './components/NewItems'
+import FeaturesSecond from './components/FeaturesSecond';
+import Products from './components/Products';
+import Testimonial from './components/Testimonial'
+import Newsletter from './components/Newsletter'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+const App = () => {
+  return <div className='w-full max-w-[1440px] mx-auto bg-white'>
+    <Header />
+    <Hero />
+    <Features />
+    <NewItems />
+    <FeaturesSecond />
+    <Products />
+    <Testimonial />
+    <Newsletter />
+    <Footer />
+  </div>;
+};
 
 export default App;
